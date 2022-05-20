4d1d74e224de575c2809fbc7f25bb7c973cd1aba
adambenaceur <benaceur.adam@gmail.com>
2022-05-20 13:22:55 -0600

    adding server index.js boilerplate code

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const cors = require("cors")
+const express = require("express")
+// TODO: add a stripe key
+const stripe = require("stripe")("")
+const uuid = require("uuid/v4")
+
+const app = express();
+
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+//routes
+app.get("/", (req, res) => {
+    res.send('working !')
+})
+
+//listen
+
+app.listen(8282, () => console.log("listening at port 8282"))
